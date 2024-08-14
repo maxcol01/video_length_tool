10e0530043b793014ce93ffffe49ec9437b41433
maxcol01 <159482922+maxcol01@users.noreply.github.com>
2024-08-14 21:33:24 +0200

    add clear file function

--- main.py
+++ main.py
@@ -10,17 +10,27 @@ def ask_input():
     return video_name, video_duration_min, video_duration_sec
 
 
+def clear_file():
+    with open('video_list.txt', 'w') as initial_file:
+        content = initial_file.read()
+        if content in initial_file:
+            initial_file.write('')
+
+
 is_target_reached = True
 titles_list = list()
 total_sum = 0
 
 while is_target_reached:
+    clear_file()
     video = ask_input()
     if total_sum + sum(video[1:]) < TARGET:
         total_sum += sum(video[1:])
+
     else:
         is_target_reached = False
     with open('video_list.txt', 'a') as file:
         file.write(f'{video[0]}\n\n')
 
-print(total_sum)
+print(f'Video time for the day: {total_sum} [s]')
+print(f'Video time for the day: {total_sum / 3600} [h]')
